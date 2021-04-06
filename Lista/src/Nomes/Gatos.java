8570da68f1e865c7ea9d907f7192fccc767c1c8e
marcos.francisco <marcos.francisco@PC04541.sjc.sp.gov.br>
2021-04-06 15:35:38 -0300

    commit inicial

---
+++ Lista/src/Nomes/Gatos.java
@@ -0,0 +1,28 @@
+package Nomes;
+
+import java.util.ArrayList;
+
+public class Gatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		ArrayList<String> gatos = new ArrayList();
+		
+		gatos.add("persa");
+		gatos.add("siames");
+		gatos.add("angora");
+		gatos.add("thai");
+		
+		for (Object nome : gatos) {
+			
+			System.out.println(nome);
+			
+			
+		}
+		
+		
+	}
+
+}
